bbb69284f421581002dfbcb19a45c9674f6b0417
MarielRodriguez072 <rodriguezmariel072@gmail.com>
2025-10-06 10:33:48 -0300

    escucha del profe uwu

--- compilador/src/main/python/Escucha.py
+++ compilador/src/main/python/Escucha.py
@@ -1,14 +1,77 @@
+from antlr4 import TerminalNode
+from antlr4 import ErrorNode
 from compiladorParser import compiladorParser
 from compiladorListener import compiladorListener
 
-
 class Escucha (compiladorListener) :
-    def enterPrograma(self, ctx:Parser.ProgramaContext) :
+    indent = 1
+    declaracion = 0
+    profundidad = 0
+    numNodos = 0
+
+    def enterPrograma(self, ctx:compiladorParser.ProgramaContext):
         print("Comienza el parsing")
-        
-    def exitPrograma(self, ctx:Parser.ProgramaContext) :
+
+    def exitPrograma(self, ctx:compiladorParser.ProgramaContext):
         print("Fin del parsing")
-    
-    def enterDeclaracion(self, ctx:compiladorParser.DeclaracionContext):
-        self.declaracion +=1
+
+    def enterIwhile(self, ctx:compiladorParser.IwhileContext):
+        print("  "*self.indent + "Comienza while")
+        self.indent += 1
         
+
+    def exitIwhile(self, ctx:compiladorParser.IwhileContext):
+        self.indent -= 1
+        print("  "*self.indent + "Fin while")
+
+    def enterDeclaracion(self, ctx:compiladorParser.DeclaracionContext):
+        self.declaracion += 1
+        print("Declaracion ENTER -> |" + ctx.getText() + "|")
+        print("  -- Cant. hijos = " + str(ctx.getChildCount()))
+    
+    def exitDeclaracion(self, ctx:compiladorParser.DeclaracionContext):
+        print("Declaracion EXIT  -> |" + ctx.getText() + "|")
+        print("  -- Cant. hijos = " + str(ctx.getChildCount()))
+    
+    def enterIif(self, ctx:compiladorParser.IifContext):
+        print(" "*self.indent + "comienza el baile del if")
+        self.indent +=1
+        
+    def exitIif(self, ctx:compiladorParser.IifContext):
+        self.indent -= 1
+        print("  "*self.indent + "Fin del baile del if")
+        
+    def enterIelse(self, ctx):
+        return super().enterIelse(ctx)
+    
+        
+    def enterFuncion(self, ctx:compiladorParser.FuncionContext):
+      print(" "*self.indent + "comienza el baile de la funcion")
+      self.indent +=1
+        
+    def exitFuncion(self, ctx:compiladorParser.FuncionContext):
+        self.indent -= 1
+        print("  "*self.indent + "Fin del baile de la funcion")
+    
+    def enterListavar(self, ctx:compiladorParser.ListavarContext):
+        self.profundidad += 1
+
+    def exitListavar(self, ctx:compiladorParser.ListavarContext):
+        print("  -- ListaVar(%d) Cant. hijos  = %d" % (self.profundidad, ctx.getChildCount()))
+        self.profundidad -= 1
+        if ctx.getChildCount() == 4 :
+            print("      hoja ID --> |%s|" % ctx.getChild(1).getText())
+
+    # def visitTerminal(self, node: TerminalNode):
+    #     print(" ---> Token: " + node.getText())
+        # self.numTokens += 1
+    
+    def visitErrorNode(self, node: ErrorNode):
+        print(" ---> ERROR")
+        
+    def enterEveryRule(self, ctx):
+        self.numNodos += 1
+    
+    def __str__(self):
+        return "Se hicieron " + str(self.declaracion) + " declaraciones\n" + \
+                "Se visitaron " + str(self.numNodos) + " nodos"
